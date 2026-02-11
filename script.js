bb0b7b93b622ffe1cefee9ceacded22187aa0de0
Anna Clara Neri <118402813+AnnaClaraNeri@users.noreply.github.com>
2026-02-11 10:13:15 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Calculadora simples
+
+function soma(a, b) {
+    return a + b;
+}
+
+function media(a, b, c) {
+    return (a + b + c) / 3;
+}
+
+console.log("Soma:", soma(5, 3));
+console.log("Média:", media(7, 8, 9));
+
+
+// Tabuada
+for (let i = 1; i <= 10; i++) {
+    console.log("5 x " + i + " = " + (5 * i));
+}
